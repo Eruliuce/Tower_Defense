ae09d2ed3a8fcb893dbabca7b1d765281c5393ab
Eruliuce <arnaudbriant@hotmail.fr>
2015-01-16 15:55:44 +0100

    Classe Joueur

--- Tower_Defense_Sources/core/src/Jeu/Joueur.java
+++ Tower_Defense_Sources/core/src/Jeu/Joueur.java
@@ -1,6 +1,22 @@
 package Jeu;
 
+import Outils.LecteurFichiers;
+
 public class Joueur
 {
-	private int pdv;
+	private int nbVies;
+	private String nom;
+	private int or;
+	
+	public Joueur(String nom)
+	{
+		nbVies = LecteurFichiers.getInt("FichiersConf\\Constantes.conf", "vieBaseJoueur");
+		this.nom = nom;
+		or = LecteurFichiers.getInt("FichiersConf\\Constantes.conf", "orBaseJoueur");
+	}
+	
+	public void perdreVies(int nb)
+	{
+		nbVies = nbVies - nb > 0 ? nbVies - nb : 0;
+	}
 }
