e7cd77d2688bbcff13a92e47144d5638a63d19de
Eruliuce <arnaudbriant@hotmail.fr>
2015-02-21 00:59:17 +0100

    Début du Controleur reseau

--- Tower_Defense_Sources/core/src/Reseau/Controleur_reseau.java
+++ Tower_Defense_Sources/core/src/Reseau/Controleur_reseau.java
@@ -1,8 +1,40 @@
 package Reseau;
 
-public class Controleur_reseau
+import java.io.IOException;
+import java.net.*;
+
+import javax.xml.stream.events.StartDocument;
+
+
+public class Controleur_reseau implements Runnable
 {
-	/*
-	@TODO
-	*/
+	private final int PORT = 10666;
+	private ServerSocket servSock;
+	private Socket sock;
+
+	public Controleur_reseau()
+	{
+		try
+		{
+			servSock = new ServerSocket(PORT);
+			start();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			sock = servSock.accept();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 }
