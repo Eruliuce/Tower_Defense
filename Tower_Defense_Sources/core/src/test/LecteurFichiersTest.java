d6a84c609c77284eec079e81ae6e94055f5ffc2c
Eruliuce <arnaudbriant@hotmail.fr>
2015-02-25 23:07:04 +0100

    tests LecteurFichiers

--- Tower_Defense_Sources/core/src/test/LecteurFichiersTest.java
+++ Tower_Defense_Sources/core/src/test/LecteurFichiersTest.java
@@ -17,13 +17,13 @@ public class LecteurFichiersTest
 	@Test
 	public void testLectureConstantes()
 	{
-		assertEquals(100, LecteurFichiers.getInt("..\\desktop\\FichiersConf\\Constantes.conf", "vieBaseJoueur"));
+		assertEquals(100, LecteurFichiers.getInt("FichiersConfTest\\Constantes.conf", "vieBaseJoueur"));
 	}
 	
 	@Test
 	public void testLectureMonstres()
 	{
-		ArrayList<ModeleMonstre> listeMonstres = LecteurFichiers.getListeModelesMonstres("..\\desktop\\FichiersConf\\Monstres.conf");
+		ArrayList<ModeleMonstre> listeMonstres = LecteurFichiers.getListeModelesMonstres("FichiersConfTest\\Monstres.conf");
 		assertEquals(5, listeMonstres.size());
 		assertEquals(2, (int)listeMonstres.get(0).getAttaque());
 		assertEquals(10, (int)listeMonstres.get(3).getRecompense());
@@ -33,7 +33,7 @@ public class LecteurFichiersTest
 	@Test
 	public void testLectureTourelles()
 	{
-		ArrayList<ModeleTourelle> listeTourelle = LecteurFichiers.getListeModelesTourelles("..\\desktop\\FichiersConf\\tourelles.conf");
+		ArrayList<ModeleTourelle> listeTourelle = LecteurFichiers.getListeModelesTourelles("FichiersConfTest\\Tourelles.conf");
 		assertEquals(5, listeTourelle.size());
 		assertEquals(2, (int)listeTourelle.get(0).getVitesseAttaque());
 		assertEquals("Tourelle anti-aerienne", listeTourelle.get(4).getName());
